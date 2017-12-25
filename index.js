cbe8d39857d08da1d901c5f2cda4b529a216cec5
fire9 <fire9@me.com>
2017-12-26 01:49:18 +0800

    change Content type format

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello, Drone!</h1');
 })
 
 server.listen(port, hostname, () => {
